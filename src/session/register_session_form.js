0ded430b505539ab73071c2b7805fb1d5b64caab
kessie <kayp3477@gmail.com>
2023-10-31 11:30:06 +0000

    Add Register session component with user registration form

--- src/session/register_session_form.js
+++ src/session/register_session_form.js
@@ -4,3 +4,30 @@ import { useDispatch } from 'react-redux';
 import { userRegister } from '../redux/user_slice';
 import ErrorMessage from '../components/ErrorMessage';
 import '../styles/form.scss';
+
+const RegisterSession = ({ btnName }) => {
+  const [nameData, setNameData] = useState('');
+  const [error, setError] = useState('');
+  const dispatch = useDispatch();
+
+  return (
+    <div className="form-container">
+      <form action="" onSubmit={(e) => formSubmissionHandler(e)}>
+        <input
+          type="text"
+          value={nameData}
+          onChange={(e) => textInputHandler(e)}
+          placeholder="Enter your name..."
+        />
+        {error && <ErrorMessage message={error} />}
+        <button type="submit">{btnName}</button>
+      </form>
+    </div>
+  );
+};
+
+RegisterSession.propTypes = {
+  btnName: PropTypes.string.isRequired,
+};
+
+export default RegisterSession;
