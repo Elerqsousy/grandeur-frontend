79be8728bd236180323d9b982927bb34a8870a36
kessie <kayp3477@gmail.com>
2023-10-31 11:32:01 +0000

    Handle user registration form submission and dispatch userRegister action.

--- src/session/register_session_form.js
+++ src/session/register_session_form.js
@@ -10,6 +10,39 @@ const RegisterSession = ({ btnName }) => {
   const [error, setError] = useState('');
   const dispatch = useDispatch();
 
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+    if (!nameData) {
+      setError('Username cannot be empty');
+      return;
+    }
+
+    dispatch(userRegister(nameData))
+      .then((response) => {
+        if (response.payload && response.payload.id) {
+          const userInfo = {
+            id: response.payload.id,
+            name: response.payload.name,
+          };
+          return userInfo;
+        }
+        throw new Error('Response payload or id is undefined.');
+      })
+      .then((userInfo) => {
+        sessionStorage.setItem('status', 'true');
+        sessionStorage.setItem('logged_user', JSON.stringify(userInfo));
+        window.location.replace('/');
+      })
+      .catch(() => {
+        setError('Registration failed. User already exists');
+      });
+  };
+
+  const textInputHandler = (e) => {
+    setNameData(e.target.value);
+    setError('');
+  };
+
   return (
     <div className="form-container">
       <form action="" onSubmit={(e) => formSubmissionHandler(e)}>
