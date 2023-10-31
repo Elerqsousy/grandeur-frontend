a77e6c0929f7fdf94f322a6c947cdac89ca0e646
kessie <kayp3477@gmail.com>
2023-10-31 10:54:59 +0000

    Implement user login async action with error handling.

---
+++ src/redux/user_slice.js
@@ -0,0 +1,16 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const API_URL = 'http://127.0.0.1:3000';
+
+export const userLogin = createAsyncThunk('user/userLogin', async (name) => {
+  try {
+    const response = await axios.post(
+      `${API_URL}/users/login`,
+      { name },
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to Login up. No user Found!');
+  }
+});
