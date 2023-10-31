1c88656920049b408165d67d1e58aa7f7a0f3d11
kessie <kayp3477@gmail.com>
2023-10-31 11:52:36 +0000

    Implement splash screen component with handlers.

--- src/components/splash_screen.js
+++ src/components/splash_screen.js
@@ -3,3 +3,41 @@ import LoginSession from '../session/login_session_form';
 import RegisterSession from '../session/register_session_form';
 import '../styles/splash_screen.scss';
 import '../styles/form.scss';
+
+const SplashScreen = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+
+  const handleLoginClick = () => {
+    setShowLogin(true);
+    setShowRegister(false);
+  };
+
+  const handleRegisterClick = () => {
+    setShowLogin(false);
+    setShowRegister(true);
+  };
+
+  const handleSwitchToLogin = () => {
+    setShowLogin(true);
+    setShowRegister(false);
+  };
+
+  const handleSwitchToRegister = () => {
+    setShowLogin(false);
+    setShowRegister(true);
+  };
+
+  return (
+    <section className="splash">
+      <div className="splash_inner">
+        <h1 className="text-focus-in">Grandeur</h1>
+        <p className=" text text-focus-in">Redefining Real Estate Reservations.</p>
+        <div className="btn slide-in-bottom">
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SplashScreen;
