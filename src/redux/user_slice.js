251025d9815ce8b0cd6020e074a80ef2ea65473c
kessie <kayp3477@gmail.com>
2023-10-31 10:56:17 +0000

    Create user registration async action with error handling.

--- src/redux/user_slice.js
+++ src/redux/user_slice.js
@@ -14,3 +14,16 @@ export const userLogin = createAsyncThunk('user/userLogin', async (name) => {
     throw new Error('Failed to Login up. No user Found!');
   }
 });
+
+export const userRegister = createAsyncThunk('user/register', async (name) => {
+  try {
+    const response = await axios.post(`${API_URL}/users`, {
+      user: {
+        name,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to sign up. Please try again.');
+  }
+});
