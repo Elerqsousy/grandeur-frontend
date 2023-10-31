05590899f784d159de67af66c856a6444762da50
kessie <kayp3477@gmail.com>
2023-10-31 11:09:26 +0000

    Implement user login form and error handling.

--- src/session/login_session_form.js
+++ src/session/login_session_form.js
@@ -4,3 +4,30 @@ import { useDispatch } from 'react-redux';
 import { userLogin } from '../redux/user_slice';
 import ErrorMessage from '../components/ErrorMessage';
 import '../styles/form.scss';
+
+const LoginSession = ({ btnName }) => {
+  const [nameData, setNameData] = useState('');
+  const [error, setError] = useState('');
+  const dispatch = useDispatch();
+
+  return (
+    <div className="form-container">
+      <form action="" onSubmit={(e) => loginFormSubmissionHandler(e)}>
+        <input
+          type="text"
+          value={nameData}
+          onChange={(e) => textInputHandler(e)}
+          placeholder="Enter your name..."
+        />
+        {error && <ErrorMessage message={error} />}
+        <button type="submit">{btnName}</button>
+      </form>
+    </div>
+  );
+};
+
+LoginSession.propTypes = {
+  btnName: PropTypes.string.isRequired,
+};
+
+export default LoginSession;
