f74253d6978bcda350e53473756f06e4b2c46d1c
kessie <kayp3477@gmail.com>
2023-10-31 11:46:55 +0000

    Add neccessary imports

---
+++ src/components/splash_screen.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import LoginSession from '../session/login_session_form';
+import RegisterSession from '../session/register_session_form';
+import '../styles/splash_screen.scss';
+import '../styles/form.scss';
