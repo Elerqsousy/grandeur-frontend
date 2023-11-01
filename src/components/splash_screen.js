b9edc72af38855aa0051927a30739783087c77e7
kessie <kayp3477@gmail.com>
2023-11-01 14:57:12 +0000

    Removed redundant code to follow the DRY principle

--- src/components/splash_screen.js
+++ src/components/splash_screen.js
@@ -1,12 +1,25 @@
 import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 import LoginSession from '../session/login_session_form';
 import RegisterSession from '../session/register_session_form';
+
 import '../styles/splash_screen.scss';
 import '../styles/form.scss';
 
+const isUserLoggedIn = () => {
+  const status = sessionStorage.getItem('status');
+  return status === 'true';
+};
+
 const SplashScreen = () => {
+  const isLoggedIn = isUserLoggedIn();
+
   const [showLogin, setShowLogin] = useState(false);
   const [showRegister, setShowRegister] = useState(false);
+
+  if (isLoggedIn) {
+    return <Navigate to="/" />;
+  }
 
   const handleLoginClick = () => {
     setShowLogin(true);
@@ -18,56 +31,56 @@ const SplashScreen = () => {
     setShowRegister(true);
   };
 
-  const handleSwitchToLogin = () => {
-    setShowLogin(true);
-    setShowRegister(false);
-  };
-
-  const handleSwitchToRegister = () => {
-    setShowLogin(false);
-    setShowRegister(true);
-  };
-
   return (
     <section className="splash">
       <div className="splash_inner">
         <h1 className="text-focus-in">Grandeur</h1>
-        <p className=" text text-focus-in">Redefining Real Estate Reservations.</p>
+        <p className=" text text-focus-in">
+          Redefining Real Estate Reservations.
+        </p>
         <div className="btn slide-in-bottom">
           <div className="splash-container">
             {showLogin && (
-            <div>
-              <LoginSession btnName="Login" />
-              <p>
-                New here?
-                {' '}
-                <button id="btn" type="button" onClick={handleSwitchToRegister}>
-                  Register
-                </button>
-              </p>
-            </div>
+              <div>
+                <LoginSession btnName="Login" />
+                <p>
+                  New here?
+                  {' '}
+                  <button className="btn" type="button" onClick={handleRegisterClick}>
+                    Register
+                  </button>
+                </p>
+              </div>
             )}
             {showRegister && (
-            <div>
-              <RegisterSession btnName="Register" />
-              <p>
-                Already have an account?
-                {' '}
-                <button id="btn" type="button" onClick={handleSwitchToLogin}>
-                  Login
-                </button>
-              </p>
-            </div>
+              <div>
+                <RegisterSession btnName="Register" />
+                <p>
+                  Already have an account?
+                  {' '}
+                  <button className="btn" type="button" onClick={handleLoginClick}>
+                    Login
+                  </button>
+                </p>
+              </div>
             )}
             {!showLogin && !showRegister && (
-            <section>
-              <button className="log-btn" type="button" onClick={handleLoginClick}>
-                Login
-              </button>
-              <button className="log-btn" type="button" onClick={handleRegisterClick}>
-                Register
-              </button>
-            </section>
+              <section>
+                <button
+                  className="log-btn"
+                  type="button"
+                  onClick={handleLoginClick}
+                >
+                  Login
+                </button>
+                <button
+                  className="log-btn"
+                  type="button"
+                  onClick={handleRegisterClick}
+                >
+                  Register
+                </button>
+              </section>
             )}
           </div>
         </div>
