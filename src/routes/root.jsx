2a654820a84dc94d5a71e5dd81caf05ceebcb59d
Riz <mahmoud.eleresousy@gmail.com>
2023-11-01 17:52:24 +0200

    update root file to handle children

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,18 +1,18 @@
-import axios from 'axios';
+// import axios from 'axios';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 export default function Root() {
-  const getData = () => {
-    axios.get('http://127.0.0.1:3000/units/1')
-      .then((response) => {
-        console.log(response.data.content);
-      });
-  };
+  // const getData = () => {
+  //   axios.get('http://127.0.0.1:3000/units/1')
+  //     .then((response) => {
+  //       console.log(response.data.content);
+  //     });
+  // };
 
-  React.useEffect(() => {
-    getData();
-  }, []);
+  // React.useEffect(() => {
+  //   getData();
+  // }, []);
   return (
     <>
       <div id="sidebar">
