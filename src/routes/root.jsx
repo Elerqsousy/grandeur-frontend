15fe37e2f06d8c531ab4874cfe3f5431c5ec8bce
Riz <mahmoud.eleresousy@gmail.com>
2023-11-02 11:24:30 +0200

    Remove API demo call

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,18 +1,7 @@
-// import axios from 'axios';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 export default function Root() {
-  // const getData = () => {
-  //   axios.get('http://127.0.0.1:3000/units/1')
-  //     .then((response) => {
-  //       console.log(response.data.content);
-  //     });
-  // };
-
-  // React.useEffect(() => {
-  //   getData();
-  // }, []);
   return (
     <>
       <div id="sidebar">
