2dd679430a7199b15bcb638e1f80c10dccda25be
SAZZAD1Q2 <kurulia115@gmail.com>
2023-11-03 19:30:50 +0600

    fix linters

--- src/components/detailPage.jsx
+++ src/components/detailPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const DetailPage = ({ image }) => (
   <div>
     <img src={image} alt="Detail" />
-
+    {/* Other component content */}
   </div>
 );
+
+DetailPage.propTypes = {
+  image: PropTypes.string.isRequired,
+};
 
 export default DetailPage;
