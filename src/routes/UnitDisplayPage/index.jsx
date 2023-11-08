3866aa72e22c9ca02e0e0c35f40aa38c32c4b3bc
Riz <mahmoud.eleresousy@gmail.com>
2023-11-08 12:43:59 +0200

    extract data from store and lick it to UI

--- src/routes/UnitDisplayPage/index.jsx
+++ src/routes/UnitDisplayPage/index.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import api from '../../redux/api';
 
 const UnitDisplayPage = () => {
+  const dispatch = useDispatch();
   const { unitId } = useParams();
-  const [id] = React.useState(unitId);
+
+  const { unitData } = useSelector((state) => state.unit);
+
+  React.useEffect(() => {
+    dispatch(api.fetchUnit(unitId));
+  }, [dispatch, unitId]);
+
+  React.useEffect(() => {
+    console.log(unitData);
+  }, [unitData]);
 
   return (
     <div>
       <h1>
         unit id
         {' '}
-        {id}
       </h1>
     </div>
   );
